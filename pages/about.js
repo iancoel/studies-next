cc86080dc7657e7323903d23add4e40d4552e273
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-02 18:14:35 -0300

    Routes & Pages

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        fugiat eaque ipsam nulla voluptatum quasi asperiores sunt qui nesciunt
+        aut distinctio quod debitis, nostrum praesentium neque ipsa porro nam
+        placeat.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        fugiat eaque ipsam nulla voluptatum quasi asperiores sunt qui nesciunt
+        aut distinctio quod debitis, nostrum praesentium neque ipsa porro nam
+        placeat.
+      </p>
+    </div>
+  );
+};
+
+export default About;
