86a7203be8c0b70c40c38bae6224e6015595432c
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-03 14:33:12 -0300

    Creating a layout component

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Navbar from '../comps/Navbar';
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas delectus
@@ -22,7 +21,6 @@ export default function Home() {
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
