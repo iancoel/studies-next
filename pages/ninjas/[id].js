d11a83ec9e66303386c03b37063042031105fad4
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-06 18:21:54 -0300

    Dynamic routes 2

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,19 @@
+export const getStaticPaths = async () => {
+  const response = await fetch('http://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
   return (
     <div>
