e875953e1dae1ef1ae666b36a424b015f97124e5
Ian Coelho <ian.coelho@meliuz.com.br>
2022-02-06 15:37:06 -0300

    Redirecting users

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(1)
+      router.push('/');
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Oops...</h1>
